00a709f4b5ea9ea52a1aa5068354bd20d1f22cd9
nasrin jafari <nasrin.jafari770@gmail.com>
2023-09-25 18:43:43 +0330

    design channel details

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -3,7 +3,6 @@ import { Box, CardContent, CardMedia, Typography } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
-
 const ChannelCard = ({ channelDetail, marginTop }) => (
   <Box
     sx={{
@@ -59,5 +58,4 @@ const ChannelCard = ({ channelDetail, marginTop }) => (
     </Link>
   </Box>
 );
-
 export default ChannelCard;
