05d2b155d3b8ab18994e96e5d8434ae6fee8a637
nasrin jafari <nasrin.jafari770@gmail.com>
2023-09-25 18:47:37 +0330

    design layout and logic videos

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import { Stack, Box } from "@mui/material";
-
 import { ChannelCard, Loader, VideoCard } from "./";
-
 const Videos = ({ videos, direction, alignItems }) => {
   if (!videos?.length) return <Loader />;
-
   return (
     <Stack
       direction={direction || "row"}
